600073dd42be3a58a19741b5fa896e78f7776cec
Terrence <netceer112@gmail.com>
2020-09-16 15:25:53 +0100

    feat: create basic landing page
    Co-authored-by: Jennifer B <jennifer.sesaybarnes@gmail.com>

---
+++ src/components/landing/index.js
@@ -0,0 +1,33 @@
+//import whatever we need
+import React from "react";
+import "./style.css";
+
+export const LandingPage = ({landing, setLanding}) => {
+  
+  return (
+    <>
+      <h1>WELCOME TO THE COOL COCKTAIL-GUESSING COOL APP!</h1>
+      <img className="landing-image" src="https://placekitten.com/200/300" alt="" />
+      <p>You will be presented with a list of options for a given cocktail.</p>
+      <p>Correctly guess all of them to win a point!</p>
+      <button onClick={() => setLanding(!landing)}>START</button>
+    </>
+  );
+};
+
+
+
+//one big landing component
+//put following three components in:
+//welcome- heading
+//logo- svg
+//instructions- p tag
+//button to start
+
+//export the big landing component
+
+// export const CardImage = ({ url }) => (
+//     <figure className="card-image--container">
+//       <img className="card-image--img" src={url} alt="" />
+//     </figure>
+//   );
